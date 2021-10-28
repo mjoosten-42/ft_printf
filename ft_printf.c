9ea974b00de04158096224a1e50d73c5f216d376
Maarten Joosten <mjoosten@f0r3s6.codam.nl>
2021-10-28 13:29:02 +0200

    Added headers

--- ft_printf.c
+++ ft_printf.c
@@ -1,19 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/28 13:15:55 by mjoosten          #+#    #+#             */
+/*   Updated: 2021/10/28 13:18:23 by mjoosten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/libft.h"
+#include <stdarg.h>
+
+static void	ft_percent(char **format);
 
 int	ft_printf(const char *format, ...)
 {
 	int	i;
 
+	if (!format)
+		return (0);
 	i = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			// Something &format
+			ft_percent(&format);
 		}
 		else
 			ft_putchar_fd(format[i], 1);
 		i++;
 	}
 	return (i);
+}
+
+static void	ft_percent(char **format)
+{
+	if (*(format + 1) == 'c')
+		va_arg
 }
